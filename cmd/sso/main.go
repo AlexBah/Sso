53c0d042058308ca035b601726ad4c93a7b7b900
Alex Bah <P_osts@mail.ru>
2025-09-09 20:51:03 +0700

    delete legacy

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -11,51 +11,20 @@ import (
 	"golang.org/x/exp/slog"
 )
 
-/*
-const (
-
-	envLocal = "local"
-	envDev   = "dev"
-	envProd  = "prod"
-
-)
-*/
 func main() {
 	cfg := config.MustLoad()
 	log := setuplogger.Setup(cfg.Env)
 	log.Info("starting application")
 
 	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TlsPath, cfg.TokenTTL)
-
 	go application.GRPCSrv.MustRun()
 
 	// Graceful shutdown
-
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
-
 	sign := <-stop
 
 	log.Info("stopping application", slog.String("signal", sign.String()))
-
 	application.GRPCSrv.Stop()
-
 	log.Info("application stopped")
 }
-
-/*
-func setupLogger(env string) *slog.Logger {
-	var log *slog.Logger
-
-	switch env {
-	case envLocal:
-		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
-	case envDev:
-		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
-	case envProd:
-		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
-	}
-
-	return log
-}
-*/
